63fccf5a58ed707665eb6ebc2c182edff460139c
saranshnema <35158960+saranshnema@users.noreply.github.com>
2020-03-06 12:05:34 -0500

    Following changes have been made - 1) Card Class - Commented the code

--- src/WAR/Card.py
+++ src/WAR/Card.py
@@ -1,13 +1,34 @@
+"""
+This is Card class that helps create an object of each playing card in a given deck.
+This object will have a Suit and a card value.
+"""
+
+
 class Card:
-  def __init__(self, suit, face_value):
-    self.__suit = suit
-    self.__face_value = face_value
+    def __init__(self, suit, face_value):
+        """
+        Constructor that initialize with following values
+        :param suit: Enum from Suit Enum class
+        :param face_value: Can range from 1-13 both included
+        """
+        self.__suit = suit
+        self.__face_value = face_value
 
-  def get_suit(self):
-    return self.__suit
+    def get_suit(self):
+        """
+        :return: Returns suit of a card
+        """
+        return self.__suit
 
-  def get_face_value(self):
-    return self.__face_value
+    def get_face_value(self):
+        """
+        :return: Returns value of a given card
+        """
+        return self.__face_value
 
-  def __str__(self):
-    return '{} of {}'.format(self.__face_value, self.__suit.name)
+    def __str__(self):
+        """
+        Example - 6 of SPADE
+        :return: String representation of a Object.
+        """
+        return '{} of {}'.format(self.__face_value, self.__suit.name)
